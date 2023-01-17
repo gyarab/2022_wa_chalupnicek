814359020651835acdaa7eaf5db2eb275679c9aa
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2023-01-17 11:25:35 +0100

    zacatek programu pro aktualni kurz eura

---
+++ prvni_python/kurz.py
@@ -0,0 +1,10 @@
+import httpx
+
+url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
+
+res = httpx.get(url)
+
+rows = res.text.split("\n")
+
+print(rows)
+# TODO
